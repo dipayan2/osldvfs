1916b9f152a43d684d216c5239d5d85712b106a5
dipayan2 <dipayan2@illinois.edu>
2022-09-06 19:51:42 -0500

    Data

---
+++ clustering/cluster_multi.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+from sklearn import metrics
+from scipy.stats import spearmanr
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+
+DATA_PATH = "./powerDVFS.csv"
+
+NUM_CLUSTER = 4
+col_max = {} #stores the max value of the column
+NU = 0.5 # importance of power
+
+#Eddie, Ryan, Sam
+def get_cmap(n, name='hsv'):
+    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct 
+    RGB color; the keyword argument name must be a standard mpl colormap name.'''
+    return plt.cm.get_cmap(name, n)
+
+def get_data():
+    df = pd.read_csv(DATA_PATH)
+    # normalized the data to range 0-1
+    for feat_name in df.columns:
+        col_max[feat_name] = df[feat_name].max()
+        df[feat_name] = df[feat_name].transform(lambda x: x/x.max())
+    
+    return df
+
+def pca_analysis(df):
+    # enter the selected list of columns to perform PCA analysis
+    df_select = df[['Mem','CPU','GPU']]
+
+
+def clustering_data(df):
+    # we need to find a way to get clustering based on 
+    df['EDP'] = df['MemPower']*pow(df["Mem"],NU)+ df['CPUPower']*pow(df["CPU"],NU)+  df['GPUPower']*pow(df["GPU"],NU)
+    kmeanModel = KMeans(n_clusters=NUM_CLUSTER).fit(df[['Mem','CPU','GPU']])
+    df['cluster'] = kmeanModel.labels_.tolist()
+    return df
+
+def print_cluster(df):
+    cluster = [ [] for i in range(NUM_CLUSTER)]
+    for idx, row in df.iterrows():
+        cluster[int(row['cluster'])].append((row['CPUBig']*col_max['CPUBig'], row['CPUSmall']*col_max['CPUSmall'], row['MemFreq']*col_max['MemFreq'], row['GPUFreq']*col_max['GPUFreq'], row['EDP']))
+    
+    for cl_idx in range(NUM_CLUSTER):
+        for val in cluster[cl_idx]:
+            print(cl_idx, val[0], val[1], val[2], val[3])
+    
+
+
+if __name__ == "__main__":
+    df = get_data()
+    df = clustering_data(df)
+    print_cluster(df)
+
+    # run something in here
