dfea4759adb82b18c0b27598179e6d216d4d0b58
dipayan2 <dipayan2@illinois.edu>
2024-09-11 11:09:50 -0500

    CPU functions operational

--- JetsonTX2/goVcode/test_code.cpp
+++ JetsonTX2/goVcode/test_code.cpp
@@ -11,7 +11,13 @@
 int main(int argc, char** argv){
     CpuClock myCPU(0);
     std::cout<< "The CPU Utilization " << myCPU.GetUtilization() << std::endl;
+    std::cout<< "The CPU Clock: " << myCPU.GetClock() << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     std::cout<< "The CPU Utilization: " << myCPU.GetUtilization() << std::endl;
+    myCPU.SetClock(1113600);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    std::cout<< "The CPU Utilization " << myCPU.GetUtilization() << std::endl;
+    std::cout<< "The CPU Clock: " << myCPU.GetClock() << std::endl;
+    
     return 0;
 }
